d1cb381a2a31fe82033855a92f51ef2df9c9aacd
mpatajac <patajacmatija3@gmail.com>
2020-09-25 19:16:03 +0200

    Move functionality to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// TODO: isprobat s _alarmom, doraditi volume change
+
 import 'package:flutter/material.dart';
 import 'package:android_alarm_manager/android_alarm_manager.dart';
 import 'package:assets_audio_player/assets_audio_player.dart';
@@ -23,75 +25,54 @@ void alarm() async {
 }
 
 void main() async {
-  final int alarmID = 0;
-  final DateTime time = new DateTime(2020, 9, 17, 12, 50);
-
   WidgetsFlutterBinding.ensureInitialized();
   await AndroidAlarmManager.initialize();
 
   runApp(ProgressiveAlarm());
 
-  await AndroidAlarmManager.oneShotAt(time, alarmID, alarm);
-
-  
+  // await AndroidAlarmManager.oneShotAt(time, alarmID, alarm);
 }
 
 class ProgressiveAlarm extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Progressive Alarm',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: AlarmSetup(title: 'Flutter Demo Home Page'),
+      home: AlarmSetup(),
     );
   }
 }
 
 class AlarmSetup extends StatefulWidget {
-  AlarmSetup({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _AlarmSetupState createState() => _AlarmSetupState();
 }
 
 class _AlarmSetupState extends State<AlarmSetup> {
-  int _counter = 0;
+  bool _alarmSet;
+  int _alarmID;
+  TimeOfDay _time;
+  DateTime _alarmTime;
+  static AssetsAudioPlayer audioPlayer;
+  static int _initialVolume;
 
   @override
   void initState() {
     super.initState();
-    initAudioStreamType();
+    _alarmSet = false;
+    _alarmID = 0;
+    _time = TimeOfDay(hour: 9, minute: 0);
+    _alarmTime = DateTime.now();
+    initAudio();
   }
 
-  void initAudioStreamType() async {
+  void initAudio() async {
     await Volume.controlVolume(AudioManager.STREAM_MUSIC);
+    _initialVolume = await Volume.getVol;
   }
 
   static void volume() async {
@@ -101,7 +82,6 @@ class _AlarmSetupState extends State<AlarmSetup> {
     /// save initial volume
     /// and set volume to 1 (minimum)
     int volume = 1;
-    final int initialVolume = await Volume.getVol;
     await Volume.setVol(volume);
 
     /// Progressively increase volume
@@ -117,71 +97,152 @@ class _AlarmSetupState extends State<AlarmSetup> {
 
     /// Reset volume and stop the player
     await Future.delayed(const Duration(seconds: 10), () {});
-    await Volume.setVol(initialVolume);
+    await Volume.setVol(_initialVolume);
     // await audioPlayer.stop();
-  }
-
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Center(child: Text('⏰\t\tProgressive Alarm\t\t⏰')),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Builder(builder: (BuildContext context) {
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  child: Text(
+                    'Select alarm time',
+                    style: TextStyle(fontSize: 28),
+                  ),
+                  onPressed: () async {
+                    TimeOfDay chosenTime = await showTimePicker(
+                        context: context, initialTime: TimeOfDay.now());
+                    setState(() {
+                      if (chosenTime != null) {
+                        _time = chosenTime;
+                        _updateAlarmTime();
+                      }
+                    });
+                  }),
+              SizedBox(height: 100),
+              Container(
+                child: Text(
+                  'Toggle alarm:',
+                  style: TextStyle(fontSize: 16),
+                ),
+                height: 30,
+              ),
+              Transform.scale(
+                scale: 2.0,
+                child: Switch(
+                  value: _alarmSet,
+                  onChanged: (bool alarmToggleState) {
+                    Scaffold.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          alarmToggleState
+                              ? 'Alarm set at ${_formatToastTime()}!'
+                              : 'Alarm stopped!',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                            height: 1.5,
+                            letterSpacing: 1.1,
+                          ),
+                        ),
+                        backgroundColor: Colors.blue,
+                      ),
+                    );
+
+                    _updateAlarmState(alarmToggleState);
+                  },
+                ),
+              )
+            ],
+          ),
+        );
+      }),
     );
+  }
+
+  String _formatToastTime() {
+    int hour = _time.hour;
+    int minute = _time.minute;
+    return '${_fixLeadingZero(hour)}:${_fixLeadingZero(minute)}';
+  }
+
+  String _fixLeadingZero(int number) {
+    return number < 10 ? '0$number' : '$number';
+  }
+
+  void _updateAlarmState(bool startAlarm) async {
+    if (startAlarm) {
+      await AndroidAlarmManager.oneShotAt(_alarmTime, _alarmID, _alarm);
+    } else {
+      if (_didAlarmStart()) {
+        await Volume.setVol(_initialVolume);
+        await audioPlayer.stop();
+      } else {
+        await AndroidAlarmManager.cancel(_alarmID);
+      }
+    }
+
+    setState(() {
+      _alarmSet = startAlarm;
+    });
+  }
+
+  bool _didAlarmStart() {
+    DateTime now = DateTime.now();
+    if (now.day < _alarmTime.day) {
+      return false;
+    }
+    if (now.hour < _alarmTime.hour) {
+      return false;
+    }
+    if (now.minute < _alarmTime.minute) {
+      return false;
+    }
+    return true;
+  }
+
+  void _updateAlarmTime() {
+    DateTime now = DateTime.now();
+    DateTime alarmTime = new DateTime(
+        now.year,
+        now.month,
+        _time.hour >= now.hour ? now.day : (now.day + 1),
+        _time.hour,
+        _time.minute);
+
+    setState(() {
+      _alarmTime = alarmTime;
+    });
+  }
+
+  static void _alarm() async {
+    /// Initalize player
+    audioPlayer = AssetsAudioPlayer();
+
+    /// Start player
+    const streamLink = "http://kepler.shoutca.st:8404/";
+    try {
+      await audioPlayer.open(Audio.liveStream(streamLink));
+    } catch (e) {
+      /// Use song as a backup
+      await audioPlayer.open(Audio("assets/audio/test.mp3"));
+      print("Stream not working.");
+    }
+
+    volume();
   }
 }
